9f8cb1e8923d7e2636096500468fb10612e4d31d
Lindy Brits <lindy.brits@immerse.io>
2020-07-21 09:11:52 +0100

    Removed Logs

--- src/courses/courses.ts
+++ src/courses/courses.ts
@@ -8,8 +8,6 @@ import { OCNCoursesRequest, OCNCoursesResponse } from "./model";
 const url = "/learning/odatav4/public/admin/ocn/v1/OcnCourses"
 
 export async function OCNPostCourses(host: string, bearerToken: string, request: OCNCoursesRequest) {
-    console.log("HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-    console.log(request);
 
     const response = await axios.post(host + url, request, {
         headers: {
@@ -18,8 +16,5 @@ export async function OCNPostCourses(host: string, bearerToken: string, request:
         }
     });
 
-    console.log("inside OCNPostCourses", response);
-    console.log("inside OCNPostCourses Data", response.data);
-
-    // return response.data as OCNCoursesResponse;
+    return response.data as OCNCoursesResponse;
 }
