9ce95c438209329687cede5851366059c74976ff
Lindy Brits <lindy.brits@immerse.io>
2020-07-30 21:16:37 +0100

    New compiled ts.

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var courses_1 = require("./courses/courses");
 exports.OCNPostCourses = courses_1.OCNPostCourses;
+var completion_1 = require("./completion/completion");
+exports.OCNCompletionStatus = completion_1.OCNCompletionStatus;
 var token_1 = require("./token/token");
 exports.OCNSignin = token_1.OCNSignin;
